35084567451caf06039881335ce9863aae403876
Shrinath Nimare <shrinath.nimare@gmail.com>
2019-02-18 20:45:08 +0530

    Fixed some stuff

--- main.cpp
+++ main.cpp
@@ -12,14 +12,16 @@ using namespace std;
 using namespace cv;
 int main()
 {
+    //this is the video device, please change this
     VideoCapture videoCap("/dev/video0");
     Mat capturedImage, regionToScan, statusZone;
-    char byt, flag, x_counter, y_counter;
+    char byt, flag;
     long longR, longG, longB;
     Scalar color(0,0,255);
-    namedWindow("wind");
+    namedWindow("Testing");
     do
     {
+        flag = 0; //this means if the color in the box is red
         videoCap >> capturedImage; //frame capture into capturedImage
         Point point1,point2; //two points
         point1.x = CORD1;
@@ -33,37 +35,41 @@ int main()
         longR = 0; //three variables for storing the total of all the RGB in the zone
         longG = 0;
         longB = 0;
-        for(x_counter = 0; x_counter < CORD2 - CORD1; x_counter++)
+        //now we'll sum all the RGB values in the area-of-interest
+        for(unsigned char x_counter = 0; x_counter < CORD2 - CORD1; x_counter++)
         {
-            for(y_counter = 0; y_counter < CORD2 - CORD1; y_counter++)
+            for(unsigned char y_counter = 0; y_counter < CORD2 - CORD1; y_counter++)
             {
                 longB += regionToScan.at<Vec3b>(x_counter, y_counter)[0];
                 longG += regionToScan.at<Vec3b>(x_counter, y_counter)[1];
                 longR += regionToScan.at<Vec3b>(x_counter, y_counter)[2];
             }
         }
+        //averaging the 3 channels
         longR /= 10000;
         longG /= 10000;
         longB /= 10000;
         printf("R%d\nG%d\nB%d\n\n",longR,longG,longB);
+        //=================================
+        //THIS IS A HORRIBLE WAY TO DO THIS
+        //=================================
+        //it works, but barely, ideally you'd want to calculate the ratio between the three colors,
+        //and ranges to what a particular color can be
         if((longR >50) &&
-                (longG < 120) &&
-                (longB < 120))
+                (longG < 50) &&
+                (longB < 50))
         {
-            printf("red detected\n");
+            printf("Red detected\n");
             flag = 1;
             statusZone = capturedImage(r,r);
         }
-        else
-        {
-            flag = 0;
-        }
+        //now we'll draw the rectangle
         if(flag == 0)
             rectangle(capturedImage,point1,point2,Scalar(0,0,0xff));
         else
             rectangle(capturedImage,point1,point2,Scalar(0,0xff,0));
-        imshow("wind",capturedImage);
+        imshow("Testing",capturedImage);
         byt = waitKey(2);
     }
-    while(byt != 'a');
+    while(byt != 'a'); //press 'a' to exit
 }
